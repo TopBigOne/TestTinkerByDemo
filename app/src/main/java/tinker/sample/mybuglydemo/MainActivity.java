35c29bd4b65d91ecba646d999a74090597b66981
guangya.zhou <chinagyzhou@gmail.com>
2022-11-16 17:23:18 +0800

    1: init the tinker base env;

---
+++ app/src/main/java/tinker/sample/mybuglydemo/MainActivity.java
@@ -0,0 +1,14 @@
+package tinker.sample.mybuglydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
